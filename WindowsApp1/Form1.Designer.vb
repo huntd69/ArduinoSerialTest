cbbf9ba79bc58bfb72759972bc1166c02f28ab51
David H <dave@huntgang.com>
2017-08-14 20:55:03 -0400

    Code being added

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(297, 115)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 145)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(297, 110)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.TextBox1.Location = New System.Drawing.Point(315, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(607, 402)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 420)
+        Me.ProgressBar1.Maximum = 1024
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(910, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 269)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(297, 144)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 455)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,154 @@
+﻿Imports System.Threading
+
+Imports System.IO
+
+Imports System.Text
+'Imports System.String
+
+
+Imports System.IO.Ports
+
+
+
+
+Public Class Form1
+
+
+    Public Shared buffer As String = ""
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim returnStr As String = "Serial Port Opened"
+
+
+        TextBox1.Text = ""
+
+
+        Try
+            SerialPort1.Open()
+        Catch ex As Exception
+            returnStr = "Error Opening Serial Port"
+        End Try
+
+
+
+        Timer1.Start()
+
+
+        TextBox1.Text = returnStr & vbCrLf
+
+
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' This is the old code
+        Dim returnStr As String = ""
+        Try
+            returnStr = SerialPort1.ReadExisting
+        Catch ex As Exception
+            'returnStr = "ERROR"
+        End Try
+        If returnStr IsNot "" Then
+            readBuffer(returnStr)
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim returnStr As String = "Serial Port Closed"
+
+        Timer1.Stop()
+        Try
+            SerialPort1.Close()
+        Catch ex As Exception
+            returnStr = "Error Closing Serial Port"
+        End Try
+        TextBox1.Text = TextBox1.Text & vbCrLf & returnStr
+
+
+
+    End Sub
+
+
+
+
+
+    Private Sub readBuffer(newString)
+
+        For counter As Int32 = 1 To Strings.Len(newString)
+            If Strings.Left(newString, 1) = vbLf Then
+                processBuffer()
+            Else
+                buffer = buffer & Strings.Left(newString, 1)
+                newString = Strings.Right(newString, (Strings.Len(newString) - 1))
+            End If
+
+        Next counter
+
+    End Sub
+
+    Private Sub processBuffer()
+        'TextBox1.Text = TextBox1.Text & buffer & vbCrLf
+        TextBox1.AppendText(buffer & vbCrLf)
+        updateProgressBar(buffer)
+        buffer = ""
+
+    End Sub
+
+    Private Sub updateProgressBar(incomingMessage As String)
+        If Strings.Left(incomingMessage, 2) = "A0" Then
+            Try
+                'ProgressBar1.Value = Strings.Mid(incomingMessage, 3)
+                If Strings.Mid(incomingMessage, 3) > 500 Then
+                    PictureBox1.BackColor = Color.Green
+                Else
+                    PictureBox1.BackColor = Color.Red
+                End If
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Private Sub OldCode()
+        ' This is the old code
+        Dim returnStr As String = ""
+        Try
+            returnStr = SerialPort1.ReadExisting
+        Catch ex As Exception
+            returnStr = "ERROR"
+        End Try
+        If returnStr IsNot "" Then
+
+            TextBox1.Text = TextBox1.Text & returnStr
+        End If
+
+    End Sub
+
+
+End Class
